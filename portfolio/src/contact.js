b52dda53794787c5d03f846be860e355aa382b23
cdeluchi <cdeluchi@gmail.com>
2022-01-07 18:00:16 +0100

    contact

--- portfolio/src/contact.js
+++ portfolio/src/contact.js
@@ -1,18 +1,22 @@
 
 function Contact() {
     return (
+      
       <div className="contact-container">
-        <div><h2>LET'S MAKE SOME CODE</h2></div>
+      {/* <div>  
+        <h2 className="h2-contact">Let's write some code</h2>  
+      </div> */}
         <div className="contact-personal">
-        <h1>Contact</h1></div>
-        <p>cdeluchi@gmail.com</p>
-        <p>(+49)0159-0177-6801</p>
+          <h1>Contact</h1>
+          <p>cdeluchi@gmail.com</p>
+          <p>(+49)0159-0177-6801</p>
+        </div>
         <div className="contact-media">
           <h1>Follow</h1>
           <p>INSTAGRAM</p>
           <p>GITHUB</p></div>
-        
       </div>
+     
     );
   }
 export default Contact;
